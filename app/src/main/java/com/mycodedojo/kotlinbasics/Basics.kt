73ec38039b01609e3a0c2d0ee19b4c6c78cfdf59
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2025-01-30 22:50:30 -0500

    variables

--- app/src/main/java/com/mycodedojo/kotlinbasics/Basics.kt
+++ app/src/main/java/com/mycodedojo/kotlinbasics/Basics.kt
@@ -1,5 +1,19 @@
 package com.mycodedojo.kotlinbasics
 
 fun main(){
-    println("Hello Mike C!")
+    var myAge = 40
+    println(myAge)
+    myAge = 44
+    println(myAge)
+
+
+val constantNumber = 10
+var mutableString = "one"
+
+    println(constantNumber)
+    println(mutableString)
+
+    mutableString = "two"
+    println(mutableString)
+
 }
---
+++ app/src/main/java/com/mycodedojo/kotlinbasics/FloatingTypes.kt
@@ -0,0 +1,8 @@
+package com.mycodedojo.kotlinbasics
+
+fun main(){
+    var circleRatio = 3.14
+    println(circleRatio)
+    circleRatio = 3.123231231231543645756875686546436464564576
+    println(circleRatio)
+}
