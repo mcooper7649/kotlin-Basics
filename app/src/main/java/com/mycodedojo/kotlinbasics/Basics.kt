c76232c62cd609ad5eb3e2e6a6dff285c7c45b78
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2025-02-01 01:10:22 -0500

    while loops

--- app/src/main/java/com/mycodedojo/kotlinbasics/Basics.kt
+++ app/src/main/java/com/mycodedojo/kotlinbasics/Basics.kt
@@ -1,23 +1,11 @@
 package com.mycodedojo.kotlinbasics
 
 fun main(){
-    var age = 0
-    // greater than >
-    // lower than <
-    // greater equal >=
-    // lower equal <=
-    // equal ==
+   var count = 0
+    while (count < 3){
+        println("Count is $count")
+        count++
+    }
 
-    println("please enter your age as a full number")
-    //read what the user entered
-    val enteredValue = readln()
-    //Convert from string to int
-    age = enteredValue.toInt()
-   if(age >= 21){
-       println("you can go into the bar")
-   }else if (age >= 18){
-       println("you can enter")
-   } else {
-       "You are too young to go into the club"
-   }
+    println("Loop is done")
 }
