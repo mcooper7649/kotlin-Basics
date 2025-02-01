e9c738d9bb1ebb0d7f86cbdceed6a562de3809d2
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2025-02-01 16:57:06 -0500

    add more logic

--- app/src/main/java/com/mycodedojo/kotlinbasics/Basics.kt
+++ app/src/main/java/com/mycodedojo/kotlinbasics/Basics.kt
@@ -1,9 +1,17 @@
 package com.mycodedojo.kotlinbasics
 
 fun main(){
-    var myResult = add(4, 10)
 
-    println(myResult)
+    println("Enter number 1")
+    val num1str = readln()
+    val num1int = num1str.toInt()
+    println("Enter number 2")
+    val num2str = readln()
+    val num2int = num2str.toInt()
+
+    var myResult = add(num1int, num2int)
+
+    println("The result is $myResult")
 }
 
 fun add(num1:Int, num2:Int):Int {
