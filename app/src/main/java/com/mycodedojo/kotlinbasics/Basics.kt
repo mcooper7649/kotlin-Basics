97ca44f1decaa683f3c6bf5dbc5efe584663017b
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2025-02-01 01:48:51 -0500

    function logic

--- app/src/main/java/com/mycodedojo/kotlinbasics/Basics.kt
+++ app/src/main/java/com/mycodedojo/kotlinbasics/Basics.kt
@@ -1,9 +1,15 @@
 package com.mycodedojo.kotlinbasics
 
 fun main(){
-    makeCoffee(3)
+    makeCoffee(1)
+    makeCoffee(4)
+    makeCoffee(10)
 }
 
 fun makeCoffee(sugarCount : Int){
-    println("Coffee with $sugarCount spoons of sugar")
+    if(sugarCount == 1) {
+        println("Coffee with $sugarCount spoon of sugar")
+    } else {
+        println("Coffee with $sugarCount spoons of sugar")
+    }
 }
