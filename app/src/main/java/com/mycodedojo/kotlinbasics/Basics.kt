69117bcfca484814f806bbb6c33ad642efc41b16
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2025-02-01 16:53:02 -0500

    fun with returns

--- app/src/main/java/com/mycodedojo/kotlinbasics/Basics.kt
+++ app/src/main/java/com/mycodedojo/kotlinbasics/Basics.kt
@@ -1,22 +1,12 @@
 package com.mycodedojo.kotlinbasics
 
 fun main(){
-    println("Who is this coffee for?")
-    val name = readln()
-    println("How much sugar would you like?")
-    val sugarCount = readln()
-    val sugarCountInt = sugarCount.toInt()
+    var myResult = add(4, 10)
 
-    makeCoffee(sugarCountInt, name)
+    println(myResult)
 }
 
-fun makeCoffee(sugarCount:Int, name:String){
-
-    if(sugarCount == 1) {
-        println("Coffee with $sugarCount spoon of sugar for $name")
-    } else if(sugarCount == 0){
-        println("Coffee with no sugar for $name")
-    } else {
-        println("Coffee with $sugarCount spoons of sugar for $name")
-    }
+fun add(num1:Int, num2:Int):Int {
+    val result = num1 + num2
+    return result
 }
