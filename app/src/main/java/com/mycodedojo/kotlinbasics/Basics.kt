ea8170d2ea34baf23401fd68394d38bc5a9293fd
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2025-01-31 20:40:30 -0500

    add unicode

--- app/src/main/java/com/mycodedojo/kotlinbasics/Basics.kt
+++ app/src/main/java/com/mycodedojo/kotlinbasics/Basics.kt
@@ -1,19 +1,7 @@
 package com.mycodedojo.kotlinbasics
 
 fun main(){
-    var myAge = 40
-    println(myAge)
-    myAge = 44
-    println(myAge)
+   val myChar = '\u00AE'
 
-
-val constantNumber = 10
-var mutableString = "one"
-
-    println(constantNumber)
-    println(mutableString)
-
-    mutableString = "two"
-    println(mutableString)
-
+    println(myChar)
 }
