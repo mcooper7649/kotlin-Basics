fa612dee5986d5ffed3516dcd4b0405fe5381138
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2025-02-01 01:36:29 -0500

    functions

--- app/src/main/java/com/mycodedojo/kotlinbasics/Basics.kt
+++ app/src/main/java/com/mycodedojo/kotlinbasics/Basics.kt
@@ -1,23 +1,12 @@
 package com.mycodedojo.kotlinbasics
 
 fun main(){
-    var age = 0
-    // greater than >
-    // lower than <
-    // greater equal >=
-    // lower equal <=
-    // equal ==
+    makeCoffee()
+}
 
-    println("please enter your age as a full number")
-    //read what the user entered
-    val enteredValue = readln()
-    //Convert from string to int
-    age = enteredValue.toInt()
-   if(age >= 21){
-       println("you can go into the bar")
-   }else if (age >= 18){
-       println("you can enter")
-   } else {
-       println("You are too young to go into the club")
-   }
+fun makeCoffee(){
+    println("Grind Beans")
+    println("Add water")
+    println("Press start")
+    println("Pour in Cup")
 }
