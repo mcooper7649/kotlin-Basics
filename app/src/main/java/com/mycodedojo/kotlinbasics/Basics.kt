68c61f479c1e2da4906172918b191ea84cf15caa
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2025-02-01 00:25:11 -0500

    if-else

--- app/src/main/java/com/mycodedojo/kotlinbasics/Basics.kt
+++ app/src/main/java/com/mycodedojo/kotlinbasics/Basics.kt
@@ -1,12 +1,18 @@
 package com.mycodedojo.kotlinbasics
 
 fun main(){
-    var age = 40
+    var age = 0
     // greater than >
     // lower than <
     // greater equal >=
     // lower equal <=
     // equal ==
+
+    println("please enter your age as a full number")
+    //read what the user entered
+    val enteredValue = readln()
+    //Convert from string to int
+    age = enteredValue.toInt()
    if(age >= 21){
        println("you can go into the bar")
    }else{
