56dc7951808d2449cb273734e96e44c01d2cca19
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2025-02-02 10:33:09 -0500

    modify object data

--- app/src/main/java/com/mycodedojo/kotlinbasics/Basics.kt
+++ app/src/main/java/com/mycodedojo/kotlinbasics/Basics.kt
@@ -1,22 +1,34 @@
 package com.mycodedojo.kotlinbasics
 
+data class CoffeeDetails(
+    val sugarCount: Int,
+    val name: String,
+    val size: String,
+    val creamAmount: Int
+)
+
 fun main(){
+    val coffeeForDenis = CoffeeDetails(0, "denis", "xxl", 0)
+    makeCoffee(coffeeForDenis)
+}
+
+fun askCoffeeDetails(){
     println("Who is this coffee for?")
     val name = readln()
     println("How much sugar would you like?")
     val sugarCount = readln()
     val sugarCountInt = sugarCount.toInt()
 
-    makeCoffee(sugarCountInt, name)
+//    makeCoffee(CoffeeDetails.sugarCountInt, CoffeeDetails.name)
 }
 
-fun makeCoffee(sugarCount:Int, name:String){
+fun makeCoffee(coffeeDetails: CoffeeDetails){
 
-    if(sugarCount == 1) {
-        println("Coffee with $sugarCount spoon of sugar for $name")
-    } else if(sugarCount == 0){
-        println("Coffee with no sugar for $name")
+    if(coffeeDetails.sugarCount == 1) {
+        println("Coffee with ${coffeeDetails.sugarCount} spoon of sugar for ${coffeeDetails.name} and ${coffeeDetails.creamAmount} cream")
+    } else if(coffeeDetails.sugarCount == 0){
+        println("Coffee with no sugar for ${coffeeDetails.name} and ${coffeeDetails.creamAmount} cream")
     } else {
-        println("Coffee with $sugarCount spoons of sugar for $name")
+        println("Coffee with ${coffeeDetails.sugarCount} spoons of sugar for ${coffeeDetails.name}and ${coffeeDetails.creamAmount} cream")
     }
 }
