3b3a545a209371895d1b3f9b34f7917d50412678
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2025-02-02 07:11:40 -0500

    fix conflicts

--- app/src/main/java/com/mycodedojo/kotlinbasics/Basics.kt
+++ app/src/main/java/com/mycodedojo/kotlinbasics/Basics.kt
@@ -1,5 +1,22 @@
 package com.mycodedojo.kotlinbasics
 
 fun main(){
-    var legend = Dog()
+    println("Who is this coffee for?")
+    val name = readln()
+    println("How much sugar would you like?")
+    val sugarCount = readln()
+    val sugarCountInt = sugarCount.toInt()
+
+    makeCoffee(sugarCountInt, name)
+}
+
+fun makeCoffee(sugarCount:Int, name:String){
+
+    if(sugarCount == 1) {
+        println("Coffee with $sugarCount spoon of sugar for $name")
+    } else if(sugarCount == 0){
+        println("Coffee with no sugar for $name")
+    } else {
+        println("Coffee with $sugarCount spoons of sugar for $name")
+    }
 }
