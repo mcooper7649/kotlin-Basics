5edff3ff8684673de2961b2c4e3cf0ffe4ff4faa
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2025-02-02 09:56:44 -0500

    complete quiz

--- app/src/main/java/com/mycodedojo/kotlinbasics/Basics.kt
+++ app/src/main/java/com/mycodedojo/kotlinbasics/Basics.kt
@@ -1,6 +1,14 @@
 package com.mycodedojo.kotlinbasics
 
 fun main() {
-    var daisy = Dog("Daisy", "Poodle", 1)
-    println("${daisy.name} is a ${daisy.breed} and is ${daisy.age} years old")
+//    var daisy = Dog("Daisy", "Poodle", 1)
+//    println("${daisy.name} is a ${daisy.breed} and is ${daisy.age} years old")
+
+    var myBook = Book()
+
+    var customBook = Book("War", "Cooper", 2021)
+
+    println("${myBook.title} by ${myBook.author} was written in ${myBook.yearPublished}")
+    println("${customBook.title} by ${customBook.author} was written in ${customBook.yearPublished}")
+
 }
---
+++ app/src/main/java/com/mycodedojo/kotlinbasics/BookClass.kt
@@ -0,0 +1,8 @@
+package com.mycodedojo.kotlinbasics
+
+class Book (val title: String = "Unknown", val author: String = "Anonymous", val yearPublished: Int = 2025){
+    init {
+
+    }
+
+}
