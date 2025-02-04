6a0358d147f4974d10f6f9f1dbd576bb7a70b562
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2025-02-03 23:00:12 -0500

    complete quiz

---
+++ app/src/main/java/com/mycodedojo/kotlinbasics/ListLooping.kt
@@ -0,0 +1,12 @@
+package com.mycodedojo.kotlinbasics
+
+fun main(){
+
+    val numbers = mutableListOf(1, 2, 3, 4, 5)
+
+    for (index in 0 until numbers.size){
+        numbers[index] = numbers[index] * 2
+    }
+
+    println(numbers)
+}
